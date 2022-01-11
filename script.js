1b906620a44dc46bf2a31d4df97aedccac678694
Layla Adil <adilayla@gmail.com>
2022-01-11 17:09:42 +0100

    parallax

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let bg = document.getElementById("bg");
+let bg = document.getElementById("background");
 let moon = document.getElementById("moon");
 let mountain = document.getElementById("mountain");
 let road = document.getElementById("road");
 let text = document.getElementById("text");
 
-window.addEventListener('srcoll', function(){
+window.addEventListener('scroll', function(){
     /*y est le nombre de pixels verticaux défilés*/
     var y = window.scrollY;
 
     bg.style.top = y * 0.5 + 'px';
-    moon.style.left = y * 0.5 + 'px';
+    moon.style.left = - y * 0.5 + 'px';
+    mountain.style.top = - y * 0.15 + 'px';
+    road.style.top = y * 0.15 + 'px';
+    text.style.top = y * 1 + 'px';
 })
 
